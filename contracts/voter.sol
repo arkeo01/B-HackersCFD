7e9b483bed01bcea5b7bfcc00b9d4185b0b1725f
arkeo <43909991+arkeo01@users.noreply.github.com>
2019-08-20 21:05:23 +0530

    alpha release
    voter contract for alpha release

---
+++ contracts/voter.sol
@@ -0,0 +1,56 @@
+pragma solidity >=0.4.21 <0.6.0;
+
+import "./zeppelin/ownable.sol";
+
+contract Voter is Ownable{                                         //password for helper component login functionality to be added
+    struct VoterDetails{
+        string name;
+        uint age;
+        uint mobileNo;
+        uint constituencyId;
+        uint aadhaarno;
+        bool voted;
+        bool canVote;
+        uint128 hashOfFingerprint;                      //unique cryptographic id of the voter to be used while voting
+    }
+
+    uint public numVoters;
+
+    mapping(address => VoterDetails) public voters;
+    address[] public voterAddresses;
+
+    event voterRegistered(
+        string name,
+        uint aadharno
+    );
+
+    function getNumOfVoters() public view returns(uint) {
+        return numVoters;
+    }
+
+    function addVoter(
+        string memory _name,
+        uint _age,
+        uint _mobileNo,
+        uint _constituencyId,
+        uint _aadharno,
+        uint128 _hashOfFingerprint
+        ) public {
+
+            voterAddresses.push(msg.sender);
+            VoterDetails memory voter = VoterDetails(
+                _name,
+                _age,
+                _mobileNo,
+                _constituencyId,
+                _aadharno,
+                false,
+                false,
+                _hashOfFingerprint
+            );
+
+            voters[msg.sender] = voter;
+            emit voterRegistered(_name, _aadharno);
+    }
+
+}
