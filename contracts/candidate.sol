e0c114a591404cb502b407c75f270079b50305e8
arkeo <43909991+arkeo01@users.noreply.github.com>
2019-08-21 00:49:54 +0530

    alpha release

--- contracts/candidate.sol
+++ contracts/candidate.sol
@@ -1,28 +1,55 @@
-pragma solidity ^0.5.7;                                  
+pragma solidity >=0.4.21 <0.6.0;
 
-import "./voterCore.sol";
+import "./voter.sol";
 
-contract Candidate is VoterCore{
-	mapping (address => Voter) public candidates;
-	uint public candidatecount;
+contract Candidate is Voter{
 
-	event candidateRegistered{
-		string _name;
-	};
+	struct CandidateDetails {
+		bytes32 party;
+		uint constituencyId;
+		bool accepted;                        //Whether the candidate is accepted to participate in elections
+	}
 
-	function addCandidate(string _name, uint _age, uint _constituencyId, uint _pincode,uint _aadharno) public{
-			candidatecount++;
-			Voter memory candidate = Voter(
-			_name,
-            _age,
-            _constituencyId,
-            _pincode,
-            _aadharno,
-            false,
-            true,
-            true
-			);
+	uint numCandidates;                     //Total Candidates
+
+	mapping (address => CandidateDetails) candidates;       //Single point access for all candidates to ensure security
+	mapping(address => uint) public votesReceived;          //votes recieved by any candidate
+
+	function addCandidate(bytes32 _party, uint _constituencyId) public {
+		numCandidates++;
+		CandidateDetails memory candidate = CandidateDetails(
+			_party,
+			_constituencyId,
+			false
+		);
 		candidates[msg.sender] = candidate;
-		emit candidateRegistered;
+	}
+
+	function getNumOfCandidates() public view returns(uint) {
+		return numCandidates;
+	}
+
+	function changeCandidateStatus(address _candidate) external {
+		CandidateDetails memory candidate = candidates[_candidate];
+		if(candidate.accepted == false)
+			candidate.accepted == true;
+		else
+			candidate.accepted == false;
+
+		candidates[_candidate] = candidate;
+	}
+
+	function getCandidate(address _candidate) public view returns (bytes memory, bytes32) {
+		CandidateDetails memory candidate = candidates[_candidate];
+		VoterDetails memory voter = voters[_candidate];
+		return (voter.name, candidate.party);
+	}
+
+	function getCandidateVotes(address _candidate) external view returns(uint){
+		return (votesReceived[_candidate]);
+	}
+
+	function voteForCandidate(address _candidate) external {			//voTokens are to be integrated
+		votesReceived[_candidate] += 1;
 	}
 }
