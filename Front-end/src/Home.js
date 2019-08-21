2b55816180aca104af42c770cdbd06700bf4dd76
Dhruv Patel <44001539+dhruv0000@users.noreply.github.com>
2019-08-21 22:37:19 +0530

    Update Home.js

--- Front-end/src/Home.js
+++ Front-end/src/Home.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
-
+import './Home.css'
 class Home extends Component {
   render() {
     return (
-      <div>
-        You're home!
+      <div class="text">
+        <section id="home" >
+        <div class="bg-text">
+          <h1>Home</h1>
+          <p>Lets Elect</p>
+        </div>
+        </section>
+        
+        <section id="about" >
+          <h1>About</h1>
+        </section>
       </div>
     );
   }
