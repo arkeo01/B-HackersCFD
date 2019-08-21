848813f540b24e15b6eb37f785d9d8761f542762
arkeo <43909991+arkeo01@users.noreply.github.com>
2019-08-21 21:00:44 +0530

    Updated utils.js
    voter and candidate reigistration functionality added

--- Front-end/src/utils.js
+++ Front-end/src/utils.js
@@ -1,14 +1,20 @@
-import voterABI from './voterABI'
-import candidateABI from './candidateABI'
-import electionABI from './electionABI'
-var Web3 = require("web3");
+import voterABI from './contractABI/voterABI'
+import candidateABI from './contractABI/candidateABI'
+import electionABI from './contractABI/electionABI'
+import tokenERC20ABI from './contractABI/tokenERC20ABI'
+import voTokenABI from './contractABI/voTokenABI'
+const Web3 = require('web3');
 // var provider = new Web3.providers.HttpProvider("https://user.blockchain.azure.com:3200/QfZMn5Qgr4NYV8EEp9wHi6fQ");
 // var web3 = new Web3(provider);
 //import Web3 from 'web3'
 let metamaskWeb3 = new Web3('http://localhost:8545')
 let account = null
-let voterContract
-let voterContractAddress = '0xf2365c97c918b3ae0074edb6d445eab6c5db5e74' // Paste Contract address here
+let voterContract, candidateContract, electionContract, tokenERC20Contract, voTokenContract
+let voterContractAddress = '0xed520b1053e81b55993f2dee132ae10d8f4d3ca5' // Paste Contract address here
+let candidateContractAddress = '0x861c9de4f7d94de838d02e936a91b694e09290aa' // Paste Contract address here
+let tokenERC20ContractAddress = '0x2bfc884b36cae581ee64b347bd8388d9296d16ec' // Paste Contract address here
+let voTokenContractAddress = '0xb3efdc1bcba042691f51b364e5ea67e6a1283338' // Paste Contract address here
+let electionContractAddress = '0xed520b1053e81b55993f2dee132ae10d8f4d3ca5' // Paste Contract address here
 
 export function web3() {
   return metamaskWeb3
@@ -18,76 +24,73 @@ export const accountAddress = () => {
   return account
 }
 
-// export async function setProvider() {
-//   // TODO: get injected Metamask Object and create Web3 instance
-//   if (window.ethereum) {
-//     metamaskWeb3 = new Web3(ethereum);
-//     try {
-//       // Request account access if needed
-//       await ethereum.enable();
-//     } catch (error) {
-//       // User denied account access...
-//       console.log(error);
-//     }
-//   }
-//   else if (window.web3) {
-//     metamaskWeb3 = new Web3(web3.currentProvider);
-//   }
-//   account = await metamaskWeb3.eth.getAccounts()
-// }
+export async function setProvider() {
+  //TODO: get injected Metamask Object and create Web3 instance
+  if (window.ethereum) {
+    metamaskWeb3 = new Web3(window.ethereum);
+    try {
+      // Request account access if needed
+      await window.ethereum.enable();
+    } catch (error) {
+      // User denied account access...
+    }
+  } else if (window.web3) {
+    metamaskWeb3 = new Web3(web3.currentProvider);
+  }
+  account = await metamaskWeb3.eth.getAccounts()
+}
 
 
-function getvoterContract() {
+function getVoterContract() {
   // TODO: create and return contract Object
   voterContract = voterContract || new metamaskWeb3.eth.Contract(voterABI.abi, voterContractAddress)
   return voterContract
 
 }
 
-function candidateContract() {
+function getCandidateContract() {
   // TODO: create and return contract Object of candidate instance
-  candidateContract = candidateContract || new metamaskWeb3.eth.Contract(candidateABI.abi, voterContractAddress)
+  candidateContract = candidateContract || new metamaskWeb3.eth.Contract(candidateABI.abi, candidateContractAddress)
   return candidateContract
 
 }
 
-function electionContract() {
+function getElectionContract() {
   // TODO: create and return contract Object of candidate instance
-  electionContract = electionContract || new metamaskWeb3.eth.Contract(electionABI.abi, voterContractAddress)
+  electionContract = electionContract || new metamaskWeb3.eth.Contract(electionABI.abi, electionContractAddress)
   return electionContract
 
 }
 
 
-
 export async function voterAdd() {
   // TODO: call voter.addvideo
-  const prop = await getvoterContract().methods.addVoter('Suresh Kumar', 45, 9024826680, 500, 491947070016, 'b64c83dbf9c97c39c').send({
+  const prop = await getVoterContract().methods.addVoter('Suresh Kumar', 45, 9024826680, 500, 491947070016, 'b64c83dbf9c97c39c').send({
     from: account[0]
   })
   alert('Voter added Sucessfully')
+  return true;
 }
 
 export async function voterNum() {
   // TODO: call voter.numVideo
-  const prop = await getvoterContract().methods.getNumOfVoters().send({
+  const prop = await getVoterContract().methods.getNumOfVoters().send({
     from: account[0]
   })
   return prop;
 }
 
-export async function candidateAdd(party,constituencyid) {
+export async function candidateAdd(party = "BJP",constituencyid = 12) {
   // TODO: call addcandidate from candidate.sol
-  const prop = await candidateContract().methods.addCandidate(party, constituencyid).send({
-    from: account[0],
-    value: 1,
+  const prop = await getCandidateContract().methods.addCandidate(party, constituencyid).send({
+    from: account[0]
   })
   alert('Candidate added Successfully')
 }
 
 export async function candidateNum() {
   // TODO: call candidateNum from the candidate.sol
-  const prop = await getcandidateContract().methods.getNumOfCandidates().send({
+  const prop = await getCandidateContract().methods.getNumOfCandidates().send({
     from: account[0]
   })
   return prop;
@@ -95,7 +98,7 @@ export async function candidateNum() {
 
 export async function constituencyAdd(constituencyid, name) {
   // TODO: to add constituency function from election.sol
-  const prop = await electionContract().methods.createConstituency(constituencyid, name).send({
+  const prop = await getElectionContract().methods.createConstituency(constituencyid, name).send({
     from: account[0],
     value: 1,
   })
@@ -104,7 +107,7 @@ export async function constituencyAdd(constituencyid, name) {
 
 export async function voterValidation(voteraddress) {
   // TODO: call addcandidate from candidate.sol
-  const prop = await electionContract().methods.validateVoter(voteraddress).send({
+  const prop = await getElectionContract().methods.validateVoter(voteraddress).send({
     from: account[0],
     value: 1,
   })
@@ -112,7 +115,7 @@ export async function voterValidation(voteraddress) {
 
 export async function votertoconstituency(constituencyid,voteraddress) {
   // TODO: call voter to constituency from election.sol
-  const prop = await electionContract().methods.addVoterToConstituency(constituencyid,voteraddress).send({
+  const prop = await getElectionContract().methods.addVoterToConstituency(constituencyid,voteraddress).send({
     from: account[0],
     value: 1,
   })
@@ -120,7 +123,7 @@ export async function votertoconstituency(constituencyid,voteraddress) {
 
 export async function candidatetoconstituency(constituencyid, candidateaddress) {
   // TODO: call addCandidateToConstituency form election.sol
-  const prop = await electionContract().methods.addCandidateToConstituency(constituencyid, candidateaddress).send({
+  const prop = await getElectionContract().methods.addCandidateToConstituency(constituencyid, candidateaddress).send({
     from: account[0],
     value: 1,
   })
@@ -128,7 +131,7 @@ export async function candidatetoconstituency(constituencyid, candidateaddress) 
 
 export async function totalVotes(candidateaddress, constituencyid) {
   // TODO: get total votes from election.sol
-  const prop = await electionContract().methods.totalVotesFor(candidateaddress, constituencyid).send({
+  const prop = await getElectionContract().methods.totalVotesFor(candidateaddress, constituencyid).send({
     from: account[0],
     value: 1,
   })
@@ -137,7 +140,7 @@ export async function totalVotes(candidateaddress, constituencyid) {
 
 export async function candidateVotes(candidateaddress, constituencyid) {
   // TODO: get total votes from election.sol
-  const prop = await electionContract().methods.voteForCandidate(candidateaddress, constituencyid).send({
+  const prop = await getElectionContract().methods.voteForCandidate(candidateaddress, constituencyid).send({
     from: account[0],
     value: 1,
   })
@@ -150,7 +153,7 @@ export async function candidateVotes(candidateaddress, constituencyid) {
 // export async function fetchAllVoters() {
 //   // TODO: call voter.propertyId
 
-//   const totalVoter = await getvoterContract().methods.voterNos().call()
+//   const totalVoter = await getVoterContract().methods.voterNos().call()
 //   //
 //   // // iterate till property Id
 //      const voters = []
